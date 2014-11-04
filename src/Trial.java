de25b25c12e4942af163574bc69e54913c8d982e
paytheo <omas.abdullah@gmail.com>
2014-11-03 22:19:28 -0500

    More massive overhauls
    * One more massive overhaul commit will be the last one needed

--- src/Trial.java
+++ src/Trial.java
@@ -15,17 +15,11 @@
  */
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Random;
 
 public class Trial
 {
-	private int[] m_aRawEntries = new int[20];
 	private int[] m_aEntries = new int[20];
 	private long[] m_aTimers = new long[20];
-	private int[] m_aIntraMirror = new int[4];
-	private int m_iIntraSwapMirrorCount;
-	private int m_iAvgInterSwitchTime;
-	private int m_iAvgIntraSwitchTime;
 	private long m_iFastestTime;
 	
 	Trial()
@@ -33,128 +27,18 @@ public class Trial
 		for (int i = 0; i < m_aEntries.length; i++)
 			m_aTimers[i] = 0;
 		
-		m_iAvgInterSwitchTime = 0;
-		m_iAvgIntraSwitchTime = 0;
-		m_iIntraSwapMirrorCount = 0;
 		GenerateTrials();
 	}
 	
-	/* PRIVATE FUNCTIONS */
 	private void GenerateTrials()
 	{
-		ArrayList<Integer> MirrorSwapNumbers = new ArrayList<Integer>();
 		ArrayList<Integer> EntryNumbers = new ArrayList<Integer>();
 		
-		for (int i = 1; i < 9; i++)
-			MirrorSwapNumbers.add(i);
-		
-		// Add Interswaps as 0s
-		for (int i = 1; i < 9; i++)
-			EntryNumbers.add(0);
-		
-		// Add Intraswaps as 1s
-		for (int i = 1; i < 12; i++)
-			EntryNumbers.add(1);
+		for (int i = 0; i < 36; i++)
+			EntryNumbers.add(i);
 			
 		Collections.shuffle(EntryNumbers);
-		Collections.shuffle(MirrorSwapNumbers);
-		
-		for (int i = 0; i < m_aIntraMirror.length; i++)
-			m_aIntraMirror[i] = MirrorSwapNumbers.get(i);
-		
-		EntryNumbers.add(0, 0);
-		
-		for (int i = 0; i < m_aEntries.length; i++)
-		{
-			m_aRawEntries[i] = EntryNumbers.get(i);
-			m_aEntries[i] = EntryNumbers.get(i);
-		}
-
-		for (int i = 0; i < m_aEntries.length; i++)
-		{
-			if (i == 0)
-			{
-				if (new Random().nextInt(2) == 0)
-					m_aEntries[i] = RandomFingerRight(0);
-				else m_aEntries[i] = RandomFingerLeft(0);
-			}
-			else
-			{
-				if (isLeftHand(m_aEntries[i-1]))
-				{
-					if (isInterSwap(m_aEntries[i]))
-					{
-						m_iIntraSwapMirrorCount++;
-						boolean isMirror = false;
-						
-						for (int j = 0; j < m_aIntraMirror.length; j++)
-						{
-							if (m_aIntraMirror[j] == m_iIntraSwapMirrorCount)
-							{
-								m_aEntries[i] = m_aEntries[i-1]*-1;
-								isMirror = true;
-							}
-						}
-						
-						if (!isMirror)
-							m_aEntries[i] = RandomFingerRight(m_aEntries[i-1]);
-					}
-					else
-						m_aEntries[i] = RandomFingerLeft(m_aEntries[i-1]);
-				}
-				else
-				{
-					if (isInterSwap(m_aEntries[i]))
-					{
-						m_iIntraSwapMirrorCount++;
-						boolean isMirror = false;
-						
-						for (int j = 0; j < m_aIntraMirror.length; j++)
-						{
-							if (m_aIntraMirror[j] == m_iIntraSwapMirrorCount)
-							{
-								m_aEntries[i] = m_aEntries[i-1]*-1;
-								isMirror = true;
-							}
-						}
-						
-						if (!isMirror)
-							m_aEntries[i] = RandomFingerLeft(m_aEntries[i-1]);
-					}
-					else
-						m_aEntries[i] = RandomFingerRight(m_aEntries[i-1]);
-				}
-			}
-		}
 	}
-	private int RandomFingerRight(int previousID)
-	{
-		int newFinger;
-		
-		if (previousID < 0)
-			previousID *= -1;
-		
-		do	newFinger = new Random().nextInt(4)+1;
-		while (newFinger == previousID);
-		
-		return newFinger;
-	}
-	private int RandomFingerLeft(int previousID)
-	{
-		int newFinger;
-		
-		if (previousID < 0)
-			previousID *= -1;
-		
-		do	newFinger = (new Random().nextInt(4)+1);	
-		while (newFinger == previousID);
-		
-		return newFinger*-1;
-	}
-	private boolean isLeftHand(int id)	{ return id < 0 ? true : false; }
-	private boolean isInterSwap(int id)	{ return id == 0 ? true : false; }
-	
-	/* PUBLIC FUNCTIONS */
 	public void setTimer(int step, long time)
 	{
 		m_aTimers[step] = time;
@@ -171,23 +55,13 @@ public class Trial
 						m_iFastestTime = m_aTimers[i];
 				}
 			}
-			
-			for (int i = 1; i < m_aRawEntries.length; i++)
-			{
-				if (m_aRawEntries[i] == 1)
-					m_iAvgIntraSwitchTime += m_aTimers[i];
-				else if (m_aRawEntries[i] == 0)
-					m_iAvgInterSwitchTime += m_aTimers[i];
-			}
-			
-			m_iAvgInterSwitchTime /= 8;
-			m_iAvgIntraSwitchTime /= 11;
 		}
 	}
-	public int getCurrentFinger(int index)
+	public int getElementAt(int index)
 	{
 		return m_aEntries[index];
 	}
+	public int getSize() {return m_aEntries.length;}
 	public String ExportTrial()
 	{
 		String exportString = "Timings: ";
@@ -200,7 +74,7 @@ public class Trial
 				exportString += m_aTimers[i] + ", ";
 		}
 		
-		exportString += "Fingers: ";
+		exportString += "Entries: ";
 		
 		for (int i = 0; i < m_aEntries.length; i++)
 		{
@@ -209,20 +83,8 @@ public class Trial
 			else
 				exportString += m_aEntries[i] + ", ";
 		}
-		
-		exportString += "Raw Fingers: ";
-		
-		for (int i = 1; i < m_aRawEntries.length; i++)
-		{
-			if (i == 19)
-				exportString += m_aRawEntries[i] + "\r\n";
-			else
-				exportString += m_aRawEntries[i] + ", ";
-		}
 
 		return exportString;
 	}
-	public int getAvgInterSwitchTime() {return m_iAvgInterSwitchTime;}
-	public int getAvgIntraSwitchTime() {return m_iAvgIntraSwitchTime;}
 	public long getFastestTime()	{return m_iFastestTime;}
 }
--- src/drawWindow.java
+++ src/drawWindow.java
@@ -18,11 +18,9 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.Vector;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 class drawWindow extends JPanel implements MouseListener
 {
@@ -32,20 +30,16 @@ class drawWindow extends JPanel implements MouseListener
 	private static final long serialVersionUID = 1L;
 	private static final int m_iCircleDiameter = 100;
 	private static final int STATE_POSITION = 105;
-	private static final int TOTAL_TRIALS = 36;
 
 	Dimension screenSize;
 	RenderingHints rh;
 	
 	State m_State;
 	State m_RecoveryState;
-	Vector<Trial> m_vGeneratedTrials;
-	Trial m_CurrentTrial;
-	int m_iCurrentTrial;
+	Trial m_Trial;
 	int m_iCurrentTrialStep;
 	
 	Timer m_Timer;
-	Button startButton;
 	Button restartButton;
 	Button quitButton;
 	Button saveButton;
@@ -53,25 +47,19 @@ class drawWindow extends JPanel implements MouseListener
 	AudioClip correctSound;
 	
 	long m_lStartTime;
-	JTextField m_TextBox;
 	int m_iGlobalTimer;
 	
 	public drawWindow()
 	{
-		m_TextBox = new JTextField(10);
 		screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		setBackground(Color.BLACK);
 		setSize((int)screenSize.getWidth(),(int)screenSize.getHeight());
 		addMouseListener(this);
-		add(m_TextBox);
-		
-		m_vGeneratedTrials = new Vector<Trial>();
 
 		try {
-			startButton = new Button(ImageIO.read(getClass().getResource("images/startButton.png")), 5, 5);
-			restartButton = new Button(ImageIO.read(getClass().getResource("images/restartButton.png")), 100, 5);
-			quitButton = new Button(ImageIO.read(getClass().getResource("images/quitButton.png")), 195, 5);
-			saveButton = new Button(ImageIO.read(getClass().getResource("images/saveButton.png")), 290, 5);
+			restartButton = new Button(ImageIO.read(getClass().getResource("images/restartButton.png")), 5, 5);
+			quitButton = new Button(ImageIO.read(getClass().getResource("images/quitButton.png")), 100, 5);
+			saveButton = new Button(ImageIO.read(getClass().getResource("images/saveButton.png")), 195, 5);
 			correctSound = Applet.newAudioClip(getClass().getResource("sounds/correctSound.wav"));
 		} catch (IOException e) {e.printStackTrace();}
         
@@ -83,18 +71,11 @@ class drawWindow extends JPanel implements MouseListener
 		if (m_Timer != null)
 			m_Timer.cancel();
 		
-		m_State = State.IDLE;
-		m_Timer = new Timer();
-		m_vGeneratedTrials.clear();
-		m_iCurrentTrial = 0;
+		m_State = State.READY;
+		m_Trial = new Trial();
+		m_Timer = new Timer();;
 		m_iCurrentTrialStep = 0;
 		m_iGlobalTimer = 0;
-
-		for (int i = 0; i < TOTAL_TRIALS; i++)
-		{
-			Trial myTrial = new Trial();
-			m_vGeneratedTrials.add(myTrial);
-		}
 	}
     private void doDrawing(Graphics g)
     {
@@ -109,22 +90,22 @@ class drawWindow extends JPanel implements MouseListener
         
         switch(m_State)
         {
-	        case IDLE:
-	        	g2d.drawImage(startButton.getImage(), startButton.getX(),  startButton.getY(),  null);
-	        	break;
-	        case FINGER_TRACKING:
-	        	g2d.drawString("FINGER TRACKING", 5, STATE_POSITION);
+	        case READY:
 	        	break;
 	        case COUNTDOWN:
 	        	g2d.drawString("Countdown to begin in " + m_iGlobalTimer + " seconds", 5, STATE_POSITION);
-	        case IN_TRIAL:
-	        	break;
 	        case COMPLETED:
 	        	g2d.drawString("The test is complete! Thank you for participating!", 5, STATE_POSITION);
 	        	break;
+		case FINGER_PRESSED:
+			break;
+		case WAIT_FOR_PRESS:
+			break;
+		default:
+			break;
         }
         
-        g2d.drawString("CURRENT TRIAL: " + (m_iCurrentTrial + 1) + "/40", 5, 75);
+        g2d.drawString("CURRENT TARGET: " + (m_iCurrentTrialStep + 1) + "/36", 5, 75);
         g2d.drawImage(restartButton.getImage(), restartButton.getX(), restartButton.getY(), null);
         g2d.drawImage(quitButton.getImage(), quitButton.getX(), quitButton.getY(), null);
         g2d.drawImage(saveButton.getImage(), saveButton.getX(), saveButton.getY(), null);
@@ -151,12 +132,12 @@ class drawWindow extends JPanel implements MouseListener
 				}
 				else
 				{
-					m_Timer.schedule(new updateTask(State.IN_TRIAL), 1000);
+					m_Timer.schedule(new updateTask(State.WAIT_FOR_PRESS), 1000);
 					m_iGlobalTimer--;
 				}
 			}
 			
-			if (m_State == State.IN_TRIAL)
+			if (m_State == State.WAIT_FOR_PRESS)
 			{
 				m_lStartTime = new Date().getTime();
 				updateTrial();
@@ -181,33 +162,16 @@ class drawWindow extends JPanel implements MouseListener
 		int x = e.getX();
 		int y = e.getY();
 		
-		if (startButton.isPressed(x, y))		{StartSimulation();}
-		else if (quitButton.isPressed(x, y))	{System.exit(0);}
+		if (quitButton.isPressed(x, y))	{System.exit(0);}
 		else if (restartButton.isPressed(x,  y)){Reset();}
 		else if(saveButton.isPressed(x,  y))	{ExportFile();}
 		else
 		{
 			switch(m_State)
 	        {
-		        case FINGER_TRACKING:
+		        case WAIT_FOR_PRESS:
 		        {
-	        	}
-		        case IN_TRIAL:
-		        {
-		        	m_CurrentTrial = m_vGeneratedTrials.get(m_iCurrentTrial);
-		        	int fingerIndex = m_CurrentTrial.getCurrentFinger(m_iCurrentTrialStep);
-		        	
-		        	switch (fingerIndex)
-		        	{
-		    	    	case -4:CheckClick(x, y, 0);	break;
-		    	    	case -3: CheckClick(x, y, 1);	break;
-		    	    	case -2: CheckClick(x, y, 2);	break;
-		    	    	case -1: CheckClick(x, y, 3);	break;
-		    	    	case 1: CheckClick(x, y, 4);	break;
-		    	    	case 2: CheckClick(x, y, 5);	break;
-		    	    	case 3: CheckClick(x, y, 6);	break;
-		    	    	case 4: CheckClick(x, y, 7);	break;
-		        	}
+		        	CheckClick(x, y, m_Trial.getElementAt(m_iCurrentTrialStep));
 	        		break;
 	        	}
 			default:
@@ -223,41 +187,35 @@ class drawWindow extends JPanel implements MouseListener
 		g = getGraphics();
 		paint(g);
 	}
-	private void StartSimulation()
-	{
-		m_State = State.FINGER_TRACKING;
-	}
 	private void ExportFile()
 	{
-		try
+		if (m_State == State.COMPLETED)
 		{
-			DateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd HH_mm_ss");
-			Date date = new Date();
-			String fileName = "";
-			
-			if (m_TextBox.getText().equals(""))
-				fileName = dateFormat.format(date) + "_NON_NAMED_TRIAL" + ".txt";
-			else
-				fileName = dateFormat.format(date) + "_" + m_TextBox.getText() + ".txt";
-			
-			PrintWriter writer = new PrintWriter(fileName, "UTF-8");
-			String exportString = "";
-			
-			for (int i = 0; i < m_vGeneratedTrials.size(); i++)
+			try
 			{
-				exportString += "TRIAL #" + i + "\r\n";
-				exportString += m_vGeneratedTrials.get(i).ExportTrial();
-				exportString += "Average InterSwitch Time: " + m_vGeneratedTrials.get(i).getAvgInterSwitchTime() + "\r\n";
-				exportString += "Average IntraSwitch Time: " + m_vGeneratedTrials.get(i).getAvgIntraSwitchTime() + "\r\n";
-				exportString += "Fastest Time: " + m_vGeneratedTrials.get(i).getFastestTime() + "\r\n";
-				exportString += "\r\n";
+				DateFormat dateFormat = new SimpleDateFormat("yyyy_MM_dd HH_mm_ss");
+				Date date = new Date();
+				String fileName = "";
+				
+				fileName = dateFormat.format(date) + "_NON_NAMED_TRIAL" + ".txt";
+				
+				PrintWriter writer = new PrintWriter(fileName, "UTF-8");
+				String exportString = "";
+				
+				for (int i = 0; i < m_Trial.getSize(); i++)
+				{
+					exportString += "TRIAL #" + i + "\r\n";
+					exportString += m_Trial.ExportTrial();
+					exportString += "Fastest Time: " + m_Trial.getFastestTime() + "\r\n";
+					exportString += "\r\n";
+				}
+				
+				writer.println(exportString);
+				writer.close();
 			}
-			
-			writer.println(exportString);
-			writer.close();
+			catch (FileNotFoundException e) {e.printStackTrace();}
+			catch (UnsupportedEncodingException e) {e.printStackTrace();}
 		}
-		catch (FileNotFoundException e) {e.printStackTrace();}
-		catch (UnsupportedEncodingException e) {e.printStackTrace();}
 	}
 	private void CheckClick(int x, int y, int fingerID)
 	{
@@ -268,26 +226,11 @@ class drawWindow extends JPanel implements MouseListener
 			long lEndTime = new Date().getTime();
 			long diffTime = lEndTime - m_lStartTime;
 			
-			m_CurrentTrial = m_vGeneratedTrials.get(m_iCurrentTrial);
-			m_CurrentTrial.setTimer(m_iCurrentTrialStep, diffTime);
+			m_Trial.setTimer(m_iCurrentTrialStep, diffTime);
 			
-			if (m_iCurrentTrialStep == 19)
+			if (m_iCurrentTrialStep == 35)
 			{
-				if (m_iCurrentTrial == 39)
-				{
-					m_State = State.COMPLETED;
-					ExportFile();
-				}
-				else
-				{
-					m_iCurrentTrial++;
-					m_iCurrentTrialStep = 0;
-					
-					if (m_iCurrentTrial == 19)
-						countDownToState(60, State.IN_TRIAL);
-					else
-						countDownToState(5, State.IN_TRIAL);
-				}
+				m_State = State.COMPLETED;
 			}
 			else
 			{
