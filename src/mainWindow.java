6fa80f5d9a8a877b49f4b4e8ad0a2e8fb9f4de08
Omas Abdullah <omas.abdullah@gmail.com>
2015-10-09 01:07:12 -0400

    final clean code

--- src/mainWindow.java
+++ src/mainWindow.java
@@ -1,18 +1,18 @@
 import javax.swing.JFrame;
 
-public class mainWindow extends JFrame
-{
+public class mainWindow extends JFrame {
 	private static final long serialVersionUID = 1L;
 
-	public static void main(String[] args) {new mainWindow();}
-	
-	public mainWindow()
-	{	
+	public static void main(String[] args) {
+		new mainWindow();
+	}
+
+	public mainWindow() {
 		setTitle("Project Target");
-        add(new drawWindow());
-        setExtendedState(JFrame.MAXIMIZED_BOTH); 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setVisible(true);
+		add(new drawWindow());
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setVisible(true);
 	}
 }
